2992243a1d8e861a8212765935b8c53d1c0fc142
RISHABH KALAKOTI <2016ucp1098@mnit.ac.in>
2016-12-07 18:09:13 +0000

    done unload too. just much more slow than staff's solution

--- dictionary.c
+++ dictionary.c
@@ -38,32 +38,32 @@ bool check(const char* word)
     
     for (int i = 1; i < wlen; i++)
     {
-        int index = (word[i - 1] == '\'') ? 26 : word[i - 1] - 'a';
+        int index = (word[i - 1] == '\'') ? 26 : tolower(word[i - 1]) - 'a';
         
-        if (trav -> children[index] == NULL)
+        if (trav->children[index] == NULL)
         {
             return false;
         }
         else
         {
-            trav = trav -> children[index];
+            trav = trav->children[index];
         }
     }
     
-    int index = (word[wlen - 1] == '\'') ? 26 : word[wlen - 1] - 'a';
+    int index = (word[wlen - 1] == '\'') ? 26 : tolower(word[wlen - 1]) - 'a';
     
-    if (trav -> children[index] == NULL)
+    if (trav->children[index] == NULL)
     {
         return false;
     }
     
-    trav = trav -> children[index];
-    if (trav -> is_word != true)
+    trav = trav->children[index];
+    if (trav->is_word != true)
     {
         
         return false;
     }
-    return true;   // not mispelled
+    return true;  
 }
 
 /**
@@ -89,61 +89,61 @@ bool load(const char* dictionary)
     
     for(int k = 0; k < 27; k++)
     {
-        trav -> children[k] = NULL;
+        trav->children[k] = NULL;
     }
     
-    if(trav == NULL)
+    if (trav == NULL)
     {
         return false;
     }
     
-    for (int c = fgetc(book), i = 0; c != EOF; c = fgetc(book))
+    for (int chr = fgetc(book), i = 0; chr != EOF; chr = fgetc(book))
     {
-            if(c != '\n')
+        if (chr != '\n')
+        {
+            word[i] = tolower(chr);
+            i++;
+        }
+        else
+        {
+            word[i] = '\0';
+            // add words to dictionary
+            
+            int wlen = strlen(word);
+            
+            
+            
+            // int flag = 0;
+            
+            for(int j = 0; j < wlen; j++)
             {
-                word[i] = tolower(c);
-                i++;
-            }
-            else
-            {
-                word[i] = '\0';
-                // add words to dictionary
-                
-                int wlen = strlen(word);
-                
-                
-                
-                // int flag = 0;
-                
-                for(int j = 0; j < wlen; j++)
+                int index = (word[j] == '\'') ? 26 : word[j] - 'a';
+                node* newNode = NULL;
+                if (trav->children[index] == NULL)
                 {
-                    int index = (word[j] == '\'') ? 26 : word[j] - 'a';
-                    node* newNode = NULL;
-                    if(trav -> children[index] == NULL)
-                    {
-                        newNode = malloc(sizeof(node));
-                        
-                        for(int k = 0; k < 27; k++)
-                        {
-                            newNode -> children[k] = NULL;
-                        }
-                    }
-                    else
-                    {
-                        newNode = trav-> children[index];
-                    }
+                    newNode = malloc(sizeof(node));
                     
-                    trav -> children[index] = newNode;
-                    trav = newNode;
-                    // printf("%d ", index);
+                    for(int k = 0; k < 27; k++)
+                    {
+                        newNode->children[k] = NULL;
+                    }
                 }
-                // assign is_word as true;
-                trav -> is_word = true;
-                trav = root;
-                // printf("%s\n", word);
-                i = 0;
-                book_size++;
+                else
+                {
+                    newNode = trav->children[index];
+                }
+                
+                trav->children[index] = newNode;
+                trav = newNode;
+                // printf("%d ", index);
             }
+            // assign is_word as true;
+            trav->is_word = true;
+            trav = root;
+            // printf("%s\n", word);
+            i = 0;
+            book_size++;
+        }
     }
     return true;
 }
@@ -157,11 +157,26 @@ unsigned int size(void)
     return book_size;
 }
 
+void freeit(node* ptr)
+{
+    for (int i = 0; i < 27; i++)
+    {
+        if (ptr->children[i] != NULL)
+        {
+            freeit(ptr->children[i]);
+        }
+    }
+    free(ptr);
+    return;
+}
+
 /**
  * Unloads dictionary from memory.  Returns true if successful else false.
  */
 bool unload(void)
 {
-    // TODO
-    return false;
+
+    freeit(root);
+    
+    return true;
 }
